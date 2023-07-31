04327c6004e8baefee9541e84aba7a866187b540
adrianhuber17 <adrianhuber17@gmail.com>
2023-07-31 10:35:02 -0700

    added transform function in pipeline script

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,10 @@
 from clean.rental_clean import clean_data
+from transform.rentals_transform import transform_data
 
 
 def pipeline():
     clean_data()
+    transform_data()
 
 
 if __name__ == "__main__":
