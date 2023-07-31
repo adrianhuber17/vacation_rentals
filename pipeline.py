e5c903c43c6110d8c56277503f25a2e84f9bf4c1
adrianhuber17 <adrianhuber17@gmail.com>
2023-07-30 19:24:50 -0700

    added raw transform and utils.py and main pipeline.py file

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+from clean.rental_clean import clean_data
+
+
+def pipeline():
+    clean_data()
+
+
+if __name__ == "__main__":
+    pipeline()
---
+++ utils.py
@@ -0,0 +1,31 @@
+COLUMNS = [
+    "name",
+    "res_num",
+    "created",
+    "type",
+    "status",
+    "arrive",
+    "nights",
+    "depart",
+    "guests",
+    "room_rent",
+    "owner_commission",
+    "management_commission",
+    "action",
+]
+
+dtypes_to_cast = {
+    "name": object,
+    "res_num": "int64",
+    "created": "datetime64",
+    "type": object,
+    "status": object,
+    "arrive": "datetime64",
+    "nights": "int64",
+    "depart": "datetime64",
+    "guests": object,
+    "room_rent": "float64",
+    "owner_commission": "float64",
+    "management_commission": "float64",
+    "action": object,
+}
